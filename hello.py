f151d9e306af66e4bd1dd21ca7587ec513454a0a
ranjithtechie <ranjithwebwiz@gmail.com>
2024-04-10 00:30:03 +0530

    Rectified error

--- hello.py
+++ hello.py
@@ -22,11 +22,7 @@ posts=[
     }
 ]
 @app.route("/")
-<<<<<<< HEAD
 def home():
-=======
-def hello_world():
->>>>>>> e12b2823143e5c17f9faae186344bcfb969480f8
     return render_template('home.html',posts=posts)
 
 @app.route("/about")
@@ -35,22 +31,13 @@ def about():
 
 @app.route("/register")
 def register():
-<<<<<<< HEAD
     form=RegistrationForm()
     return render_template('register.html',title='register',form=form)
-=======
-    form=RegisterForm()
-    return render_template('register.html',title='register,form=form')
->>>>>>> e12b2823143e5c17f9faae186344bcfb969480f8
 
 @app.route("/login")
 def login():
     form=LoginForm()
-<<<<<<< HEAD
     return render_template('login.html',title='Login',form=form)
-=======
-    return render_template('login.html',title='Login,form=form')
->>>>>>> e12b2823143e5c17f9faae186344bcfb969480f8
 
 if __name__ == '__main__':
     app.run(debug=True)
